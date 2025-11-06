7eaab2636ca6dda23dcd27af9aba363e342a71da
dustyxcat <smr623@naver.com>
2025-11-06 11:43:02 +0900

    sec_4 end

--- js/main.js
+++ js/main.js
@@ -2,11 +2,26 @@ $(document).ready(function(){
 
     var swiper = new Swiper(".sec_1_bg", {
         loop: true,
-        // autoplay: true,
+        autoplay: true,
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
         },
+    });
+
+    var swiper = new Swiper(".sec_4_swiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        loop: true,
+        slidesPerView: 3.2,
+        coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+
     });
 
 
